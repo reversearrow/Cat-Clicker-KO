9a11ef6ffcfe7597129b9270d49f4a5953b4791b
reversearrow <reversearrow@gmail.com>
2017-07-18 09:52:45 -0400

    Added Cat Levels

--- js/script.js
+++ js/script.js
@@ -3,8 +3,23 @@ function clickCounterViewModel() {
   this.clicks = ko.observable(0);
   this.name = ko.observable("Kitty1");
   this.imgSrc = ko.observable("img/img1.jpg");
+  this.levels = ko.observable()
+  this.computeLevels = ko.computed(function(){
+    if(this.clicks() <= 10){
+      this.levels("Infant");
+    }else if(this.clicks()>10 && this.clicks()<=20){
+      this.levels("Teen");
+    }else if(this.clicks()>20 && this.clicks()<=30){
+      this.levels("Adult");
+    }else if(this.clicks()>30 && this.clicks()<=40){
+      this.levels("Old");
+    }else if(this.clicks()>40){
+      this.levels("Dead")
+    }
+  }, this);
   this.addClick = function addClick(){
     this.clicks(this.clicks() + 1)
+
   };
 }
 
